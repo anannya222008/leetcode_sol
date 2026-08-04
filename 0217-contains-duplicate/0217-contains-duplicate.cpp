bfc4eec547c6255d184e18b6c0b148c260db0b94
Anannya <anannya222008@gmail.com>
2026-08-04 19:50:07 +0530

    Time: 27 ms (85.57%), Space: 82.8 MB (84.88%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int sz = nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<sz - 1;i++){
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+};
